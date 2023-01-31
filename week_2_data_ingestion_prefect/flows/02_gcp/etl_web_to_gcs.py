373b8816d376327e5aa5e109bb2d401fd3bc464a
yeha98555 <yeha98555@gmail.com>
2023-02-01 05:52:13 +0800

    Prefect gcp ingest_data with Blocks

---
+++ week_2_data_ingestion_prefect/flows/02_gcp/etl_web_to_gcs.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+
+    # # test retries
+    # if randint(0, 1) > 0:
+    #     raise Exception
+
+    df = pd.read_csv(dataset_url)
+    return df
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issue"""
+    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+@task()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    output_dir = Path(f'../../data/{color}')
+    if not Path.exists(output_dir):
+        output_dir.mkdir()
+    path = Path(f'{output_dir}/{dataset_file}.parquet')
+    df.to_parquet(path, compression='gzip')
+    return path
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("dtcde-gcs")
+    gcs_block.upload_from_path(
+        from_path=path,
+        to_path='/'.join(str(path).split('/')[-2:])
+    )
+
+
+@flow()
+def etl_web_to_gcs() -> None:
+    """The main ETL function"""
+    color = 'yellow'
+    year = 2021
+    month = 1
+    dataset_file = f'{color}_tripdata_{year}-{month:02}'
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
+
+
+if __name__ == '__main__':
+    etl_web_to_gcs()
