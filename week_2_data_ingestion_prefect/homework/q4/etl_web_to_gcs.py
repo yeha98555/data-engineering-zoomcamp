0fe75aeb916ccbef64ead38e80b2a83a6f305587
yeha98555 <yeha98555@gmail.com>
2023-02-06 04:42:00 +0800

    change output_dir in q4 web_to_gcs script

--- week_2_data_ingestion_prefect/homework/q4/etl_web_to_gcs.py
+++ week_2_data_ingestion_prefect/homework/q4/etl_web_to_gcs.py
@@ -28,7 +28,7 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
-    output_dir = Path(f'../../data/{color}')
+    output_dir = Path(f'data/{color}')
     if not Path.exists(output_dir):
         output_dir.mkdir(parents=True, exist_ok=True)
     path = Path(f'{output_dir}/{dataset_file}.parquet')
