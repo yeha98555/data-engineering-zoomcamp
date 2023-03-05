96172db5a55387aaeb5f8c27f32e19f4a6fce752
yeha98555 <yeha98555@gmail.com>
2023-03-05 20:58:36 +0800

    fix dim_zone type wrong words

--- week_4_analytics_engineering/taxi_rides_ny/models/core/dim_zones.sql
+++ week_4_analytics_engineering/taxi_rides_ny/models/core/dim_zones.sql
@@ -6,7 +6,7 @@
 
 select
     locationid,
-    borough
+    borough,
     zone,
     replace(service_zone, 'Boro', 'Green') as service_zone
-from {{ ref('tax_zone_lookup') }}
+from {{ ref('taxi_zone_lookup') }}
