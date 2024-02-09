320b7cf44f462aaebedbacdd77e626eab04cda7c
yeha98555 <yeha98555@gmail.com>
2024-02-10 01:49:35 +0800

    add data_to_gcs exporter

---
+++ week_2_data_ingestion_mage/mage-project/magic-zoomcamp/data_exporters/data_to_gcs.py
@@ -0,0 +1,40 @@
+import os
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+    """
+    Exports data to some source.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Output (optional):
+        Optionally return any object and it'll be logged and
+        displayed when inspecting the block run.
+    """
+
+    bucket_name = os.environ['BUCKET_NAME']
+    project_id = os.environ['PROJECT_ID']
+
+    table_name = os.environ['TABLE_NAME']
+
+    root_path = f"{bucket_name}/{table_name}"
+    
+    table = pa.Table.from_pandas(data)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols=['lpep_pickup_date'],
+        filesystem=gcs
+    )
+
